fb3322e66d2aa295d792b18f21aacaebf3c9bf4d
Truby Voglund <tvoglund@hotmail.com>
2012-09-26 21:44:03 -0600

    fixed a config setting

--- BootStrapMongoSample/DataAccess/DataAccess.cs
+++ BootStrapMongoSample/DataAccess/DataAccess.cs
@@ -8,6 +8,7 @@ using MongoDB.Driver;
 using Models;
 using System.Configuration;
 using System.Web.Management;
+using System.IO;
 
 namespace DataAccess
 {
@@ -17,25 +18,33 @@ namespace DataAccess
         {
             var modelExercises = new List<Exercise>();
 
-            var mongoConnectionString = ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString();
-            var login = ConfigurationManager.AppSettings["login"].ToString();
-            var password = ConfigurationManager.AppSettings["password"].ToString();
-            MongoServer mongo = MongoServer.Create(mongoConnectionString);
-            MongoCredentials mc = new MongoCredentials(login, password);
-
-            new LogEvent(string.Format("mongolab_uri:  {0}{1}login:  {2}{1}password{3}{1}", mongoConnectionString, Environment.NewLine, login, password)).Raise();    
-            
-
-            var workout = mongo.GetDatabase("appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6", mc);
-            //end of monglab
-            var exerciseCollection = workout.GetCollection("exercise");
-            var exercises = workout.GetCollection("exercise").FindAllAs<ExerciseInternal>().ToList();
-
-            mongo.Disconnect();
-
-            foreach (var exercise in exercises)
+            try
             {
-                modelExercises.Add(Maps.Map(exercise));
+
+                var mongoConnectionString = ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString();
+                var login = ConfigurationManager.AppSettings["login"].ToString();
+                var password = ConfigurationManager.AppSettings["password"].ToString();
+                MongoServer mongo = MongoServer.Create(mongoConnectionString);
+                MongoCredentials mc = new MongoCredentials(login, password);
+
+                new LogEvent(string.Format("mongolab_uri:  {0}{1}login:  {2}{1}password{3}{1}", mongoConnectionString, Environment.NewLine, login, password)).Raise();    
+                
+                
+                var workout = mongo.GetDatabase("appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6", mc);
+                //end of monglab
+                var exerciseCollection = workout.GetCollection("exercise");
+                var exercises = workout.GetCollection("exercise").FindAllAs<ExerciseInternal>().ToList();
+
+                mongo.Disconnect();
+
+                foreach (var exercise in exercises)
+                {
+                    modelExercises.Add(Maps.Map(exercise));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
             }
 
             return modelExercises;
