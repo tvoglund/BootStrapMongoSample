ce95ac1f92de1cdc18b266c516e2c2dddd033788
Truby Voglund <tvoglund@hotmail.com>
2012-09-26 20:45:15 -0600

    fixed a config setting

--- BootStrapMongoSample/DataAccess/DataAccess.cs
+++ BootStrapMongoSample/DataAccess/DataAccess.cs
@@ -6,6 +6,7 @@ using System.Text;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using Models;
+using System.Configuration;
 
 namespace DataAccess
 {
@@ -15,13 +16,12 @@ namespace DataAccess
         {
             var modelExercises = new List<Exercise>();
 
-            //MongoServer mongo = MongoServer.Create("mongodb://199.141.41.138:27017");
-            //MongoServer mongo = MongoServer.Create("mongodb://192.168.0.3:27017");
-            //var workout = mongo.GetDatabase("workout");
-            //below is for mongolab
-            //mongodb://appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6:lhf6utmuo44u29fv66353pd4q5@ds037607-a.mongolab.com:37607/appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6
-            MongoServer mongo = MongoServer.Create("mongodb://appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6:lhf6utmuo44u29fv66353pd4q5@ds037607-a.mongolab.com:37607/appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6");
-            MongoCredentials mc = new MongoCredentials("tvoglund", "Bela%Kavi_1");
+            var mongoConnectionString = ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString();
+            var login = ConfigurationManager.AppSettings["login"].ToString();
+            var password = ConfigurationManager.AppSettings["password"].ToString();
+
+            MongoServer mongo = MongoServer.Create(mongoConnectionString);
+            MongoCredentials mc = new MongoCredentials(login,password);
             var workout = mongo.GetDatabase("appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6", mc);
             //end of monglab
             var exerciseCollection = workout.GetCollection("exercise");
