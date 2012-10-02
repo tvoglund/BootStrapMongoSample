9475638bae76724a87ff71b8c15d81d39096031f
Truby Voglund <tvoglund@hotmail.com>
2012-10-01 21:15:12 -0600

    moved services because of app_harbor... ugh

--- BootStrapMongoSample/DataAccess/DataAccess.cs
+++ BootStrapMongoSample/DataAccess/DataAccess.cs
@@ -21,13 +21,26 @@ namespace DataAccess
             try
             {
 
-                var mongoConnectionString = ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString();
-                var login = ConfigurationManager.AppSettings["login"].ToString();
-                var password = ConfigurationManager.AppSettings["password"].ToString();
-                MongoServer mongo = MongoServer.Create(mongoConnectionString);
-                MongoCredentials mc = new MongoCredentials(login, password);
+                //var mongoConnectionString = ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString();
+                //var login = ConfigurationManager.AppSettings["login"].ToString();
+                //var password = ConfigurationManager.AppSettings["password"].ToString();
+                //MongoServer mongo = MongoServer.Create(mongoConnectionString);
+                //MongoCredentials mc = new MongoCredentials(login, password);
 
-                
+                //new LogEvent(string.Format("mongolab_uri:  {0}{1}login:  {2}{1}password{3}{1}", mongoConnectionString, Environment.NewLine, login, password)).Raise();    
+
+
+
+
+
+                //MongoServer mongo = MongoServer.Create("mongodb://199.141.41.138:27017");
+                //MongoServer mongo = MongoServer.Create("mongodb://192.168.0.3:27017");
+                //var workout = mongo.GetDatabase("workout");
+                //below is for mongolab
+                //mongodb://appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6:lhf6utmuo44u29fv66353pd4q5@ds037607-a.mongolab.com:37607/appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6
+                MongoServer mongo = MongoServer.Create("mongodb://appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6:lhf6utmuo44u29fv66353pd4q5@ds037607-a.mongolab.com:37607/appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6");
+                MongoCredentials mc = new MongoCredentials("tvoglund", "Bela%Kavi_1");
+
 
 
                 var workout = mongo.GetDatabase("appharbor_bc09b46d-eeb1-4970-b1dc-0a0774e058d6", mc);
